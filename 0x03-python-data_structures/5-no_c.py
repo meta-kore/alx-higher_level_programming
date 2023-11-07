d1c4c5c3aa57452e90d3bbaaf78010657a72313c
meta-kore <79240412+meta-kore@users.noreply.github.com>
2023-11-07 04:13:52 +0300

    last of the styles

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,4 +5,4 @@ def no_c(my_string):
     for i in my_string:
         if i is not 'c' and i is not 'C':
             new_str += i
-    return new_str # return my_string.translate({ord(c): None for c in "cC"})
+    return new_str
