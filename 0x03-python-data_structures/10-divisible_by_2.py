a47e92e55e4abb32b48fbec7b400b81a9fb36e07
meta-kore <79240412+meta-kore@users.noreply.github.com>
2023-11-07 03:58:36 +0300

    a host of style fixes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
     boolist = my_list[:]
     for count, i in enumerate(my_list):
@@ -6,4 +7,4 @@ def divisible_by_2(my_list=[]):
             boolist[count] = True
         else:
             boolist[count] = False
-    return(boolist)
+    return boolist
