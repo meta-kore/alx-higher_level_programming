09f541e677220f2b8dcc09fcaf086920918dfb29
meta-kore <79240412+meta-kore@users.noreply.github.com>
2023-11-11 05:18:20 +0300

    pycode fixes

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def complex_delete(my_dict, value):
     targets = []
     for key, key_value in my_dict.items():
@@ -6,4 +7,4 @@ def complex_delete(my_dict, value):
             targets.append(key)
     for x in targets:
         del my_dict[x]
-    return(my_dict)
+    return my_dict
